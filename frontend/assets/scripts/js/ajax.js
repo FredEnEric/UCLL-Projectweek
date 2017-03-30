7755f97532237a2fa1b4f00f2d0ee996a37a318c
indyvanmol <indyvanmol@hotmail.com>
2017-03-30 08:28:35 -0700

    last

--- frontend/assets/scripts/js/ajax.js
+++ frontend/assets/scripts/js/ajax.js
@@ -1,12 +1,43 @@
+var showAll = function (e) {
+    e.preventDefault();
+    listParts();
+};
+
 var listParts = function() {
-    return $.ajax({
+        $.ajax({
         url: "http://localhost:7050/chaincode",
         type: 'POST',
         contentType: 'application/json',
         crossDomain: true,
-        data: '{"jsonrpc": "2.0","method": "query","params": {"type": 1,"chaincodeID":{"name":"6c980af56b1eeff4da7c9eef31c5715ba99c844e65f19566b94eaa1662db3a3220b6515bc5f938d90c89ef62a2ab7c2054cd1f12621df0066344ed363e2db15a"},"ctorMsg": {"function":"listParts","args":[]}},"id": 54}'
-    });
-}
+        data: '{"jsonrpc": "2.0","method": "query","params": {"type": 1,"chaincodeID":{"name":"e588d8e9efbc23f0910a8c7399d4e470e44ff4cb7b8947783802eb16c6e11e07b300e5d9930f6d93662793829121a59c73bbaa3587aebf3301bf0aa0691712d1"},"ctorMsg": {"function":"listParts","args":[]}},"id": 54}'
+    }).done(function (result) {
+            $('#partList').empty();
+            $('#partList').append("<h2>Parts</h2>");
+            $.each(JSON.parse(result.result.message).Parts, function (i, part) {
+                var ul = "<ul>";
+                $.each(part, function (key, value) {
+                    ul += "<li>" + key + ": " + value + "</li>";
+                });
+                $('#partList').append(ul + "</ul>");
+            });
+        });
+};
+
+var showAddField = function (e) {
+    e.preventDefault();
+    $('#addField').empty().append('<form> <h2>Add a part</h2>     <label for="partID">Id</label> <input id="partID" type="text"><br> <label for="Manufacturer">Manufacturer</label> <input id="Manufacturer" type="text"><br> <label for="Notes">Notes</label> <input id="Notes" type="text"><br> <label for="PartIds">PartIds</label> <input id="PartIds" type="text"><br> <label for="Specification">Specification</label> <input id="Specification" type="text"><br> <a id="addnewpart" href="#">add</a> <a id="stop" href="#">cancel</a> </form>')
+
+};
+
+var stop = function (e) {
+    e.preventDefault();
+    $('#addField').empty();
+};
+
+var addone = function (e) {
+    e.preventDefault();
+    CreatePart($('#partID').val(), $('#Manufacturer').val(), $('#PartIds').val(), $('#Specification').val(), $('#Notes').val());
+};
 
 var CreatePart = function(Id, Manufacturer, PartIds, Specification, Notes){
     $.ajax({
@@ -14,11 +45,12 @@ var CreatePart = function(Id, Manufacturer, PartIds, Specification, Notes){
         type: 'POST',
         contentType: 'application/json',
         crossDomain: true,
-         data: '{"jsonrpc": "2.0","method": "query","params": {"type": 1,"chaincodeID":{"name":"6c980af56b1eeff4da7c9eef31c5715ba99c844e65f19566b94eaa1662db3a3220b6515bc5f938d90c89ef62a2ab7c2054cd1f12621df0066344ed363e2db15a"},"ctorMsg": {"function":"CreatePart","args":["'+Id+'","'+Manufacturer+'","'+PartIds+'","'+Specification+'","'+Notes+'"]}},"id": 55}'
+         data: '{"jsonrpc": "2.0","method": "query","params": {"type": 1,"chaincodeID":{"name":"e588d8e9efbc23f0910a8c7399d4e470e44ff4cb7b8947783802eb16c6e11e07b300e5d9930f6d93662793829121a59c73bbaa3587aebf3301bf0aa0691712d1"},"ctorMsg": {"function":"CreatePart","args":["'+Id+'","'+Manufacturer+'","'+PartIds+'","'+Specification+'","'+Notes+'"]}},"id": 55}'
     }).done(function (result) {
-        console.log(result);
+        $('#addField').empty();
+        listParts();
     });
-}
+};
 
 var GetPart = function(Id){
     $.ajax({
@@ -26,40 +58,15 @@ var GetPart = function(Id){
         type: 'POST',
         contentType: 'application/json',
         crossDomain: true,
-         data: '{"jsonrpc": "2.0","method": "query","params": {"type": 1,"chaincodeID":{"name":"6c980af56b1eeff4da7c9eef31c5715ba99c844e65f19566b94eaa1662db3a3220b6515bc5f938d90c89ef62a2ab7c2054cd1f12621df0066344ed363e2db15a"},"ctorMsg": {"function":"GetPart","args":["'+Id+'"]}},"id": 56}'
+         data: '{"jsonrpc": "2.0","method": "query","params": {"type": 1,"chaincodeID":{"name":"e588d8e9efbc23f0910a8c7399d4e470e44ff4cb7b8947783802eb16c6e11e07b300e5d9930f6d93662793829121a59c73bbaa3587aebf3301bf0aa0691712d1"},"ctorMsg": {"function":"GetPart","args":["'+Id+'"]}},"id": 56}'
     }).done(function (result) {
         console.log(result);
     });
-}
+};
 
-var UpdatePart = function(args){
-    $.ajax({
-        url: "http://localhost:7050/chaincode",
-        type: 'POST',
-        contentType: 'application/json',
-        crossDomain: true,
-         data: '{"jsonrpc": "2.0","method": "query","params": {"type": 1,"chaincodeID":{"name":"6c980af56b1eeff4da7c9eef31c5715ba99c844e65f19566b94eaa1662db3a3220b6515bc5f938d90c89ef62a2ab7c2054cd1f12621df0066344ed363e2db15a"},"ctorMsg": {"function":"UpdatePart","args":["'+args+'"]}},"id": 56}'
-    }).done(function (result) {
-        console.log(result);
-    });
-}
-
-
-
-
-
-
-
-    var myApp = angular.module("myApp", []);
-    myApp.controller("partdealer", function($scope) {
-        listParts().done(function (result) {
-            $scope.rr = (result.result.message);
-            console.log("result", result.result.message)
-            $scope.$digest();
-        })
-    })
-
-
-
-
-
+$(document).ready(function (){
+   $('#showAllParts').on('click', showAll);
+   $('#addPart').on('click', showAddField);
+   $('#addField').on('click', 'a#stop', stop);
+   $('#addField').on('click', 'a#addnewpart', addone);
+});
